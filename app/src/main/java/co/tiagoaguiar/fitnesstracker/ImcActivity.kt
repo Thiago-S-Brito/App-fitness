b1a5f9e9341fde5860445805e63719c00be8bacf
Thiago-S-Brito <thhiago369@gmail.com>
2024-08-15 18:41:37 -0300

    continuação do projeto

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,9 +1,13 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
@@ -20,15 +24,85 @@ class ImcActivity : AppCompatActivity() {
 
         val btnSend: Button = findViewById(R.id.btn_imc_send)
         btnSend.setOnClickListener {
-        if (!validate()) {
-            Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_SHORT).show()
-            return@setOnClickListener
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+
+            val result = calculateImc(weight, height)
+            Log.d("teste", "resultado: $result")
+
+            val imcResponseId = imcResponse(result)
+
+            val title = getString(R.string.imc_response, result)
+
+            AlertDialog.Builder(this)
+
+                .setTitle(getString(R.string.imc_response, result))
+                .setMessage(imcResponseId)
+                //opção 1: utilizando lambda
+                .setPositiveButton(android.R.string.ok) { dialog, which ->
+                    //aqui vai rodar depois do click
+                }
+                .create()
+                .show()
+
+
+            //opção 2: dá mais trabalho escrever
+//            dialog.setPositiveButton(android.R.string.ok, object  : DialogInterface.OnClickListener {
+//                override fun onClick(dialog: DialogInterface?, which: Int) {
+//                // aqui vai rodar depois do click
+//                }
+//
+//            })
+
+
         }
-            //aqui deu sucesso
+    }
+    @StringRes
+    private fun imcResponse(imc:Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+
         }
+//        if (imc < 15.0) {
+//            return R.string.imc_severely_low_weight
+//        } else if (imc < 16.0) {
+//            return R.string.imc_very_low_weight
+//        } else if (imc < 18.5) {
+//            return R.string.imc_low_weight
+//        } else if (imc < 25.0) {
+//            return R.string.normal
+//        } else if (imc < 30.0) {
+//            return R.string.imc_high_weight
+//        } else if (imc < 35.0) {
+//            return R.string.imc_so_high_weight
+//        } else if (imc < 40.0) {
+//            return R.string.imc_severely_high_weight
+//        }
+//          else {
+//            return R.string.imc_extreme_weight
+//        }
+    }
+
+    private fun  calculateImc(weight: Int, height: Int): Double {
+        // peso / (altura * altura) peso dividido pela altura ao quadrado
+        return weight / ( (height / 100.0) * (height / 100.0) )
     }
 
     private fun validate(): Boolean {
+
+        /*
         // não pode inserir valores nulos / vazio
         // não podde inserir/começar com sezo (0)
 
@@ -57,7 +131,7 @@ class ImcActivity : AppCompatActivity() {
 //            return true
 //        }
 //            return false
-
+*/
         //opção 3: return direto o que for verdadeiro
         return (editWeight.text.toString().isNotEmpty()
             && editHeight.text.toString().isNotEmpty()
